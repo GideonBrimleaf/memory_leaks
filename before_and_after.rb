8987a208cae2a61bd12d9a2320c9620c5b1a3df7
Chris Dryden <cdryden@imperosoftware.com>
2022-08-04 11:24:08 +0100

    Reverts to empty project

--- before_and_after.rb
+++
@@ -1,5 +0,0 @@
-require_relative "./people"
-
-print_usage_before_and_after do
-  run(ARGV.shift.to_i)
-end
--- profile.rb
+++
@@ -1,9 +0,0 @@
-require 'memory_profiler'
-require_relative 'people'
-
-
-report = MemoryProfiler.report do
-  run(1000)
-end
-
-report.pretty_print(to_file: "profile.txt")
